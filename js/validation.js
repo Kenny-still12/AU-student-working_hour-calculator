073bb11b442e00f856170b6c027e00f3a72f9a71
Kenneth <kensonoda1124@gmail.com>
2026-04-02 10:31:22 +1100

    added comments

--- js/validation.js
+++ js/validation.js
@@ -1,6 +1,7 @@
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 
 
+// checks if the pushed new shift is in the current displayed week. if it is in previous or next week, then it returns false.
 export function isSelectedWeek(shift, currentWeekOffSet) {
     const shiftDate = dayjs(shift.date);
     const targetWeek = dayjs().add(currentWeekOffSet, "week");
@@ -8,6 +9,7 @@ export function isSelectedWeek(shift, currentWeekOffSet) {
     return shiftDate.isSame(targetWeek, "week")
 }
 
+// checks if there is existing shift.
 export function hasShift(shift, shifts) {
 
     return shifts.some((existingShift) => existingShift.date === shift.date)
